e2250cd0a4466347a117bcc9ba9906da9dc8582f
alberto_clase <asanabriagarcia@safareyes.es>
2024-11-18 17:55:01 +0100

    primera version del inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {InicioComponent} from './inicio/inicio.component';
+import {QuienesSomosComponent} from './quienes-somos/quienes-somos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InicioComponent, QuienesSomosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
