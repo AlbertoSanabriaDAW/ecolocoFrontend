3d0a19e825ea12e01b3e53eb048d5a67266b4458
alberto_clase <asanabriagarcia@safareyes.es>
2024-11-19 20:29:37 +0100

    Header version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import {RouterOutlet} from '@angular/router'; // Ajusta la ruta si es necesario
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, HeaderComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ecolocoFrontend';
