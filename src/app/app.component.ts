7b9d4fd101c500befb39e9a1cc2a2dcf435a6e8c
alberto_clase <asanabriagarcia@safareyes.es>
2024-11-19 20:55:18 +0100

    Primera version del footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import {RouterOutlet} from '@angular/router'; // Ajusta la ruta si es necesario
+import {RouterOutlet} from '@angular/router';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, HeaderComponent,
+    RouterOutlet, HeaderComponent, FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
