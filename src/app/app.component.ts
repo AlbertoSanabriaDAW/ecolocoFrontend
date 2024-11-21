8bf9639c5e07fe2923191a1fa7b9be2074647782
alberto_clase <asanabriagarcia@safareyes.es>
2024-11-21 19:43:55 +0100

    visualmente tenemos un registro de usuario, falta la funcionalidad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { HeaderComponent } from './header/header.component';
 import {RouterOutlet} from '@angular/router';
 import {FooterComponent} from './footer/footer.component';
 import {InicioComponent} from './inicio/inicio.component';
+import {QuienesSomosComponent} from './quienes-somos/quienes-somos.component';
+import {RegistroSesionComponent} from './registro-sesion/registro-sesion.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, HeaderComponent, FooterComponent, InicioComponent
+    RouterOutlet, HeaderComponent, FooterComponent, InicioComponent, QuienesSomosComponent, RegistroSesionComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
