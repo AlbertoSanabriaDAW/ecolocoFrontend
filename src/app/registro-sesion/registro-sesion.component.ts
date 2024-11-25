22f249bedce55f8e408d5229c0c43b291ce95340
alberto_clase <asanabriagarcia@safareyes.es>
2024-11-25 19:46:17 +0100

    el botón de registrarse reacciona con un hover

--- src/app/registro-sesion/registro-sesion.component.ts
+++ src/app/registro-sesion/registro-sesion.component.ts
@@ -49,3 +49,46 @@ export class RegistroSesionComponent {
     }
   }
 }
+// comentario del botón funcional de registrarse que da fallos:
+// import { Component } from '@angular/core';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { CommonModule } from '@angular/common';
+// import { IonicModule } from '@ionic/angular';
+//
+// @Component({
+//   selector: 'app-registro-sesion',
+//   templateUrl: './registro-sesion.component.html',
+//   styleUrls: ['./registro-sesion.component.scss'],
+//   standalone: true,
+//   imports: [CommonModule, IonicModule],
+// })
+// export class RegistroSesionComponent {
+//   registerForm: FormGroup;
+//
+//   constructor(private fb: FormBuilder) {
+//     this.registerForm = this.fb.group(
+//       {
+//         username: ['', [Validators.required, Validators.minLength(3)]],
+//         email: ['', [Validators.required, Validators.email]],
+//         password: ['', [Validators.required, Validators.minLength(6)]],
+//         confirmPassword: ['', Validators.required],
+//       },
+//       { validators: this.passwordMatchValidator }
+//     );
+//   }
+//
+//   passwordMatchValidator(form: FormGroup) {
+//     const password = form.get('password')?.value;
+//     const confirmPassword = form.get('confirmPassword')?.value;
+//     return password === confirmPassword ? null : { passwordMismatch: true };
+//   }
+//
+//   onSubmit() {
+//     if (this.registerForm.valid) {
+//       console.log('Datos del formulario:', this.registerForm.value);
+//       alert('¡Registro exitoso!');
+//     } else {
+//       alert('Por favor, completa todos los campos correctamente.');
+//     }
+//   }
+// }
