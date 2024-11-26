1b12c83bac1374b17a140854be8cf72ea7707b52
alberto_clase <asanabriagarcia@safareyes.es>
2024-11-26 17:41:24 +0100

    Las rutas del header ahora son funcionales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { InicioComponent } from './inicio/inicio.component';
 import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
 import { RegistroSesionComponent } from './registro-sesion/registro-sesion.component';
 import {InicioSesionComponent} from './inicio-sesion/inicio-sesion.component';
+import {EventosComponent} from './eventos/eventos.component';
+import {ContactoComponent} from './contacto/contacto.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent }, // Página principal
@@ -10,5 +12,7 @@ export const routes: Routes = [
   { path: 'registro', component: RegistroSesionComponent }, // Página de registro
   { path: 'inicioSesion', component: InicioSesionComponent }, // Página de registro
   { path: 'inicio', component: InicioComponent}, // Página Inicio
+  { path: 'eventos', component: EventosComponent}, //Página Eventos
+  { path: 'contacto', component: ContactoComponent}
 
 ];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,19 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {IonHeader, IonToolbar} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [IonicModule],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [CommonModule, IonToolbar, IonHeader],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  navigateTo(page: string) {
-    console.log(`Redirigiendo a: ${page}`);
-    // Aquí puedes implementar la lógica de navegación con Angular Router en el futuro.
-    // Ejemplo:
-    // this.router.navigate([`/${page}`]);
+  constructor(private router: Router) {}
+
+  navigateTo(path: string) {
+    this.router.navigate([path]);
   }
 }
