eab22dbe71a9b0832b98c63c53ca09b090259576
alberto_clase <asanabriagarcia@safareyes.es>
2024-11-28 17:57:30 +0100

    Primera version crear evento

--- src/app/creacion-evento/creacion-evento.component.ts
+++ src/app/creacion-evento/creacion-evento.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {IonContent, IonDatetime, IonInput, IonLabel, IonTextarea} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-creacion-evento',
-  standalone: true,
-  imports: [],
   templateUrl: './creacion-evento.component.html',
-  styleUrl: './creacion-evento.component.css'
+  styleUrls: ['./creacion-evento.component.css'],
+  imports: [
+    FormsModule,
+    IonInput,
+    IonLabel,
+    ReactiveFormsModule,
+    IonContent,
+    IonTextarea,
+    IonDatetime
+  ],
+  standalone: true
 })
 export class CreacionEventoComponent {
+  eventoForm: FormGroup; // Declarar el formulario
 
+  constructor(private fb: FormBuilder) {
+    // Inicializar el formulario con controles y validaciones
+    this.eventoForm = this.fb.group({
+      foto: [''], // Campo no obligatorio
+      titulo: ['', Validators.required], // Campo obligatorio
+      descripcion: ['', Validators.required], // Campo obligatorio
+      fecha: ['', Validators.required], // Campo obligatorio
+      ubicacion: ['', Validators.required], // Campo obligatorio
+      aptitudes: ['Todos los públicos'], // Campo no obligatorio con valor por defecto
+    });
+  }
+
+  // Método que se ejecuta al enviar el formulario
+  onSubmit() {
+    if (this.eventoForm.valid) {
+      console.log('Evento creado:', this.eventoForm.value);
+      // Lógica adicional para manejar la creación del evento
+    } else {
+      console.error('Formulario inválido');
+    }
+  }
 }
