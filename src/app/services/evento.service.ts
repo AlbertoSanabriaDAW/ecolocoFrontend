96261b6a07004999cb3ed1d9acdca5787c3dcf1e
Alberto <ahernandezlopez@safareyes.es>
2024-11-28 19:15:57 +0100

    Evento service opertativo

---
+++ src/app/services/evento.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+export interface Evento {
+  id: number;
+  titulo: string;
+  descripcion: string;
+  ubicacion: string;
+  aptitudes: string[];
+  foto: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventoService {
+  private apiURL = '/api/eventos'; // URL de la API
+
+  constructor(private http: HttpClient) { }
+
+  obtenerEventos(): Observable<any> {
+    return this.http.get(`${this.apiURL}/lista`); // Obtener eventos
+  }
+}
